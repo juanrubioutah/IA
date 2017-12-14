5c2fd7de2e1fc23409826fb224a79fa4be7122bf
Juan Rubio <juanrubioutah@gmail.com>
2017-12-14 09:04:22 -0700

    ML Working!

--- Super TicTacToe/src/MLHelper.java
+++ Super TicTacToe/src/MLHelper.java
@@ -107,86 +107,248 @@ public class MLHelper {
 		printWriter.println("@data");
 		printWriter.flush();
 	}
-	public static void write() {
+	public static void write(Board board, int space) {
 		printWriter.print(Init.board1.board[1]+","+Init.board1.board[2]+","+Init.board1.board[3]+","+Init.board1.board[4]+","+Init.board1.board[5]+","+Init.board1.board[6]+","+Init.board1.board[7]+","+Init.board1.board[8]+","+Init.board1.board[9]+",");
-		printWriter.flush();
 		printWriter.print(Init.board2.board[1]+","+Init.board2.board[2]+","+Init.board2.board[3]+","+Init.board2.board[4]+","+Init.board2.board[5]+","+Init.board2.board[6]+","+Init.board2.board[7]+","+Init.board2.board[8]+","+Init.board2.board[9]+",");
-		printWriter.flush();
 		printWriter.print(Init.board3.board[1]+","+Init.board3.board[2]+","+Init.board3.board[3]+","+Init.board3.board[4]+","+Init.board3.board[5]+","+Init.board3.board[6]+","+Init.board3.board[7]+","+Init.board3.board[8]+","+Init.board3.board[9]+",");
-		printWriter.flush();
 		printWriter.print(Init.board4.board[1]+","+Init.board4.board[2]+","+Init.board4.board[3]+","+Init.board4.board[4]+","+Init.board4.board[5]+","+Init.board4.board[6]+","+Init.board4.board[7]+","+Init.board3.board[8]+","+Init.board4.board[9]+",");
-		printWriter.flush();
 		printWriter.print(Init.board5.board[1]+","+Init.board5.board[2]+","+Init.board5.board[3]+","+Init.board5.board[4]+","+Init.board5.board[5]+","+Init.board5.board[6]+","+Init.board5.board[7]+","+Init.board5.board[8]+","+Init.board5.board[9]+",");
-		printWriter.flush();
 		printWriter.print(Init.board6.board[1]+","+Init.board6.board[2]+","+Init.board6.board[3]+","+Init.board6.board[4]+","+Init.board6.board[5]+","+Init.board6.board[6]+","+Init.board6.board[7]+","+Init.board6.board[8]+","+Init.board6.board[9]+",");
-		printWriter.flush();
 		printWriter.print(Init.board7.board[1]+","+Init.board7.board[2]+","+Init.board7.board[3]+","+Init.board7.board[4]+","+Init.board7.board[5]+","+Init.board7.board[6]+","+Init.board7.board[7]+","+Init.board7.board[8]+","+Init.board7.board[9]+",");
-		printWriter.flush();
 		printWriter.print(Init.board8.board[1]+","+Init.board8.board[2]+","+Init.board8.board[3]+","+Init.board8.board[4]+","+Init.board8.board[5]+","+Init.board8.board[6]+","+Init.board8.board[7]+","+Init.board8.board[8]+","+Init.board8.board[9]+",");
-		printWriter.flush();
-		printWriter.println(Init.board9.board[1]+","+Init.board9.board[2]+","+Init.board9.board[3]+","+Init.board9.board[4]+","+Init.board9.board[5]+","+Init.board9.board[6]+","+Init.board9.board[7]+","+Init.board9.board[8]+","+Init.board9.board[9]+",TRUE");
-		printWriter.flush();
-	}
-	public static void performML() throws Exception {
-		printWriter.close();
-		BufferedReader dataFile = readDataFile("MLData.arff");
-		
-		Instances data = new Instances(dataFile);
-		data.setClassIndex(data.numAttributes() - 1);
-		
-		Instances[][] split = crossValidationSplit(data, 10);
-		
-		Instances[] trainingSplits = split[0];
-		Instances[] testingSplits = split[1];
-		
-		Classifier[] models = { new J48(), new PART(), new DecisionTable(), new DecisionStump()};
-		double accuracy = 0.0;
-		for(int i = 0; i < models.length; i++) {
-			FastVector predictions = new FastVector();
-			
-			for(int j = 0; j < trainingSplits.length; i++) {
-				Evaluation validation = classify(models[j], trainingSplits[i], testingSplits[i]);
-				
-				predictions.appendElements(validation.predictions());
-			}
-			accuracy = calculateAccuracy(predictions);
-			
-			System.out.println("ML Accuracy: "+String.format("%.2f%%",  accuracy));
+		if(determineSuccess(board, space)==true) {
+			printWriter.println(Init.board9.board[1]+","+Init.board9.board[2]+","+Init.board9.board[3]+","+Init.board9.board[4]+","+Init.board9.board[5]+","+Init.board9.board[6]+","+Init.board9.board[7]+","+Init.board9.board[8]+","+Init.board9.board[9]+",TRUE");
 		}
-		System.out.println("ML Accuracy: "+String.format("%.2f%%",  accuracy));
+		else if(determineSuccess(board, space)==false) {
+			printWriter.println(Init.board9.board[1]+","+Init.board9.board[2]+","+Init.board9.board[3]+","+Init.board9.board[4]+","+Init.board9.board[5]+","+Init.board9.board[6]+","+Init.board9.board[7]+","+Init.board9.board[8]+","+Init.board9.board[9]+",FALSE");
+		}
+		printWriter.flush();
 	}
-	public static BufferedReader readDataFile(String fileName) {
+	public static BufferedReader readDataFile(String filename) {
 		BufferedReader inputReader = null;
+ 
 		try {
-			inputReader = new BufferedReader(new FileReader(fileName));
-		} catch(FileNotFoundException e) {
-			System.out.println("ML FIle Not Found");
-			e.printStackTrace();
+			inputReader = new BufferedReader(new FileReader(filename));
+		} catch (FileNotFoundException ex) {
+			System.err.println("File not found: " + filename);
 		}
+ 
 		return inputReader;
 	}
-	public static Evaluation classify(Classifier model, Instances trainingSet, Instances testingSet) throws Exception {
+ 
+	public static Evaluation classify(Classifier model,
+			Instances trainingSet, Instances testingSet) throws Exception {
 		Evaluation evaluation = new Evaluation(trainingSet);
+ 
 		model.buildClassifier(trainingSet);
 		evaluation.evaluateModel(model, testingSet);
+ 
 		return evaluation;
 	}
+ 
 	public static double calculateAccuracy(FastVector predictions) {
 		double correct = 0;
-		for(int i = 0; i<predictions.size(); i++) {
+ 
+		for (int i = 0; i < predictions.size(); i++) {
 			NominalPrediction np = (NominalPrediction) predictions.elementAt(i);
-			if(np.predicted()==np.actual()) {
+			if (np.predicted() == np.actual()) {
 				correct++;
 			}
 		}
-		return 100*correct/predictions.size();
+ 
+		return 100 * correct / predictions.size();
 	}
-	public static Instances[][] crossValidationSplit(Instances data, int numberOfFolds){
+ 
+	public static Instances[][] crossValidationSplit(Instances data, int numberOfFolds) {
 		Instances[][] split = new Instances[2][numberOfFolds];
-		for(int i = 0; i< numberOfFolds; i++) {
+ 
+		for (int i = 0; i < numberOfFolds; i++) {
 			split[0][i] = data.trainCV(numberOfFolds, i);
 			split[1][i] = data.testCV(numberOfFolds, i);
 		}
+ 
 		return split;
+	}
+ 
+	public static void performML() throws Exception {
+		BufferedReader datafile = readDataFile("MLData.arff");
+ 
+		Instances data = new Instances(datafile);
+		data.setClassIndex(data.numAttributes() - 1);
+ 
+		Instances[][] split = crossValidationSplit(data, 10);
+ 
+		Instances[] trainingSplits = split[0];
+		Instances[] testingSplits = split[1];
+ 
+		Classifier[] models = {new J48(),new PART(),new DecisionTable(),new DecisionStump()};
+ 
+		// Run for each model
+		for (int j = 0; j < models.length; j++) {
+ 
+			// Collect every group of predictions for current model in a FastVector
+			FastVector predictions = new FastVector();
+			
+ 
+			// For each training-testing split pair, train and test the classifier
+			for (int i = 0; i < trainingSplits.length; i++) {
+				Evaluation validation = classify(models[j], trainingSplits[i], testingSplits[i]);
+ 
+				predictions.appendElements(validation.predictions());
+				System.out.println("Test OUTPUT: "+predictions.elementAt(0).toString());
+ 
+				// Uncomment to see the summary for each training-testing pair.
+				//System.out.println(models[j].toString());
+			}
+ 
+			// Calculate overall accuracy of current classifier on all splits
+			double accuracy = calculateAccuracy(predictions);
+ 
+			// Print current classifier's name and accuracy in a complicated,
+			// but nice-looking way.
+			System.out.println("Accuracy of " + models[j].getClass().getSimpleName() + ": "
+					+ String.format("%.2f%%", accuracy)
+					+ "\n---------------------------------");
+		}
+ 
+	}
+	public static boolean determineSuccess(Board lastPlayBoard, int lastPlaySpace) { //Determine whether the last move was successful
+		if(lastPlayBoard.isComplete) {
+			if(lastPlayBoard.board[1].equals("O")) {
+				return true;
+			}
+			else {
+				return false;
+			}
+		}
+		//1, 2, 3
+		if(lastPlayBoard.board[1].equals("X")&&lastPlayBoard.board[2].equals("X")) {
+			if(lastPlaySpace==3) {
+				return true;
+			}
+		}
+		if(lastPlayBoard.board[2].equals("X")&&lastPlayBoard.board[3].equals("X")) {
+			if(lastPlaySpace==1) {
+				return true;
+			}
+		}
+		if(lastPlayBoard.board[1].equals("X")&&lastPlayBoard.board[3].equals("X")) {
+			if(lastPlaySpace==2) {
+				return true;
+			}
+		}
+		//4, 5, 6
+		if(lastPlayBoard.board[5].equals("X")&&lastPlayBoard.board[6].equals("X")) {
+			if(lastPlaySpace==4) {
+				return true;
+			}
+		}
+		if(lastPlayBoard.board[4].equals("X")&&lastPlayBoard.board[5].equals("X")) {
+			if(lastPlaySpace==6) {
+				return true;
+			}
+		}
+		if(lastPlayBoard.board[4].equals("X")&&lastPlayBoard.board[6].equals("X")) {
+			if(lastPlaySpace==5) {
+				return true;
+			}
+		}
+		//7, 8, 9
+		if(lastPlayBoard.board[8].equals("X")&&lastPlayBoard.board[9].equals("X")) {
+			if(lastPlaySpace==7) {
+				return true;
+			}
+		}
+		if(lastPlayBoard.board[7].equals("X")&&lastPlayBoard.board[8].equals("X")) {
+			if(lastPlaySpace==9) {
+				return true;
+			}
+		}
+		if(lastPlayBoard.board[7].equals("X")&&lastPlayBoard.board[9].equals("X")) {
+			if(lastPlaySpace==8) {
+				return true;
+			}
+		}
+		//1, 4, 7
+		if(lastPlayBoard.board[4].equals("X")&&lastPlayBoard.board[7].equals("X")) {
+			if(lastPlaySpace==1) {
+				return true;
+			}
+		}
+		if(lastPlayBoard.board[1].equals("X")&&lastPlayBoard.board[7].equals("X")) {
+			if(lastPlaySpace==4) {
+				return true;
+			}
+		}
+		if(lastPlayBoard.board[1].equals("X")&&lastPlayBoard.board[4].equals("X")) {
+			if(lastPlaySpace==7) {
+				return true;
+			}
+		}
+		//2, 5, 8
+		if(lastPlayBoard.board[5].equals("X")&&lastPlayBoard.board[8].equals("X")) {
+			if(lastPlaySpace==2) {
+				return true;
+			}
+		}
+		if(lastPlayBoard.board[2].equals("X")&&lastPlayBoard.board[5].equals("X")) {
+			if(lastPlaySpace==8) {
+				return true;
+			}
+		}
+		if(lastPlayBoard.board[2].equals("X")&&lastPlayBoard.board[8].equals("X")) {
+			if(lastPlaySpace==5) {
+				return true;
+			}
+		}
+		//3, 6, 9
+		if(lastPlayBoard.board[3].equals("X")&&lastPlayBoard.board[6].equals("X")) {
+			if(lastPlaySpace==9) {
+				return true;
+			}
+		}
+		if(lastPlayBoard.board[6].equals("X")&&lastPlayBoard.board[9].equals("X")) {
+			if(lastPlaySpace==3) {
+				return true;
+			}
+		}
+		if(lastPlayBoard.board[3].equals("X")&&lastPlayBoard.board[9].equals("X")) {
+			if(lastPlaySpace==6) {
+				return true;
+			}
+		}
+		//1, 5, 9
+		if(lastPlayBoard.board[1].equals("X")&&lastPlayBoard.board[5].equals("X")) {
+			if(lastPlaySpace==9) {
+				return true;
+			}
+		}
+		if(lastPlayBoard.board[5].equals("X")&&lastPlayBoard.board[9].equals("X")) {
+			if(lastPlaySpace==1) {
+				return true;
+			}
+		}
+		if(lastPlayBoard.board[1].equals("X")&&lastPlayBoard.board[9].equals("X")) {
+			if(lastPlaySpace==5) {
+				return true;
+			}
+		}
+		//3, 5, 7
+		if(lastPlayBoard.board[3].equals("X")&&lastPlayBoard.board[5].equals("X")) {
+			if(lastPlaySpace==7) {
+				return true;
+			}
+		}
+		if(lastPlayBoard.board[5].equals("X")&&lastPlayBoard.board[7].equals("X")) {
+			if(lastPlaySpace==3) {
+				return true;
+			}
+		}
+		if(lastPlayBoard.board[3].equals("X")&&lastPlayBoard.board[7].equals("X")) {
+			if(lastPlaySpace==5) {
+				return true;
+			}
+		}
+		return false;
 	}
 }
